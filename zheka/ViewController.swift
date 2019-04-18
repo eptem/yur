f108b6a6e6389647322447693235a5c0c1b36f5e
Eptem <mropp1@mail.ru>
2019-04-18 22:15:25 +0500

    Initial Commit

---
+++ zheka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zheka
+//
+//  Created by Артем Жорницкий on 18/04/2019.
+//  Copyright © 2019 Артем Жорницкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
